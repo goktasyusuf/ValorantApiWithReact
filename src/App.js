131fc2deeaf28181f105cdd56beee8ef5dd00344
Yusuf Göktaş <82331935+goktasyusuf@users.noreply.github.com>
2022-12-20 17:48:22 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,66 +1,64 @@
-import React, { Component } from 'react';
-import './App.css';
-import Searchbar from './components/Searchbar';
-import AgentList from './components/AgentList';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Searchbar from "./components/Searchbar";
+import AgentList from "./components/AgentList";
+import axios from "axios";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import AboutAgent from './components/AboutAgent';
+import AboutAgent from "./components/AboutAgent";
 
- class App extends Component {
+const App = () => {
+  const [agents, setAgents] = useState([]);
 
-state = {
-  agents :  [
-     
-    ],
-    searchQuery:""
-  }
+  const [searchQuery, setSearchQuery] = useState("");
 
+  useEffect(() => {
+    const FetchData = async () => {
+      const baseURL =
+        "https://valorant-api.com/v1/agents/?isPlayableCharacter=true";
+      const response = await axios.get(baseURL);
+      const data = response.data;
+      setAgents(data.data);
+    };
+    FetchData();
+  }, []);
 
-  async componentDidMount() {
-    const baseURL ="https://valorant-api.com/v1/agents/?isPlayableCharacter=true"
-    const response =await  axios.get(baseURL);
-    const data = await response.data;
-    //console.log(data.data)
-    this.setState({agents : data.data})
-    
-  }
+  const searchMovie = (event) => {
+    setSearchQuery(event.target.value);
+  };
 
-  searchMovie = (event)=> {
-    this.setState({searchQuery : event.target.value})
-  }
-
-  render() {
-
-    let filteredAgents = this.state.agents.filter((agent)=>{
-      return agent.displayName.toLowerCase().indexOf(this.state.searchQuery.toLowerCase())!==-1;
-    })
+  let filteredAgents = agents.filter((agent) => {
     return (
-      <Router>
-        <Routes>
-          <Route path='/' element={
-            <React.Fragment>
-            <div className="container" style={{maxWidth:"1000px"}}>
-            <br />
-            <div className="row">
-              <div className="col-12">
-                <h1 className='text-center'>Valorant Wikipedia</h1>
-              </div>
-            </div>
-              <div className="row">
-                <div className="col-12">
-                <Searchbar searchMovie = {this.searchMovie}/>
-                </div>
-              </div>
-              <AgentList agents = {filteredAgents}/>
-            </div>
-            </React.Fragment>
-          }></Route>
-          <Route path='/agents/:id' element={<AboutAgent/>}></Route>
-
-  </Routes>
-  </Router>
+      agent.displayName.toLowerCase().indexOf(searchQuery.toLowerCase()) !== -1
     );
-  }
-}
+  });
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <React.Fragment>
+              <div className="container" style={{ maxWidth: "1000px" }}>
+                <br />
+                <div className="row">
+                  <div className="col-12">
+                    <h1 className="text-center">Valorant Wikipedia</h1>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-12">
+                    <Searchbar searchMovie={searchMovie} />
+                  </div>
+                </div>
+                <AgentList agents={filteredAgents} />
+              </div>
+            </React.Fragment>
+          }
+        ></Route>
+        <Route path="/agents/:id" element={<AboutAgent />}></Route>
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
